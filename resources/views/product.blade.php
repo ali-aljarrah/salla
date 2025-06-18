34e7b1ea047ae312cb309a18bff9187bf833a3cf
Ali Aljarrah <ali.jarrah.109@gmail.com>
2025-06-18 15:41:39 +0300

    Fix style

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -67,7 +67,7 @@ content="اشتري {{$product->name}} من شي تريند. . توصيل سري
     <section class="py-4">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-10 mx-auto">
                     <h2 class="secondary-title">الوقت المتبقي للخصم</h2>
                     <div>
                         <div class="countdown mt-3">
@@ -113,7 +113,7 @@ content="اشتري {{$product->name}} من شي تريند. . توصيل سري
     <section class="py-5">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-10 mx-auto">
                     <h2 class="display-5 fw-bold mb-5 title-des">وصف المنتج :</h2>
                     <div class="product-description">
                         {!! Str::markdown($product->description) !!}
@@ -127,7 +127,7 @@ content="اشتري {{$product->name}} من شي تريند. . توصيل سري
     <section class="py-5">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 mb-4">
+                <div class="col-lg-10 mx-auto mb-4">
                     <div class="choose-qty w-100 shadow-1 rounded p-2 text-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000" class="bi bi-arrow-down-short" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4"/>
